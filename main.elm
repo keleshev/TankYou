22f06a77746df9846ca08bde28cb857f78025754
Vladimir Keleshev <vladimir@keleshev.com>
2015-07-23 21:14:54 +0200

    Different friction for tank's x and y axis

--- main.elm
+++ main.elm
@@ -1,9 +1,9 @@
-import Time exposing (..)
+import Time exposing (fps)
 import Window
 import Debug
 
 import Tank exposing (Tank)
-import Vector exposing (Vector, (+.), (*.), (/.))
+import Vector exposing (Vector, (+.), (*.), (/.), (×))
 import Controls exposing (Controls)
 import View
 
@@ -12,13 +12,15 @@ import View
 -- UPDATE --
 
 update : (Float, Controls) -> Tank -> Tank
-update (dt, controls) tank =
+update (dt', controls) tank =
+  let _ = 1 / dt' |> floor |> Debug.watch "fps" in
+  let dt = min dt (1 / 10) in
   let _ = controls |> Debug.watch "controls" in
   let forces = inferForces tank controls in
   let force = sumForces forces in
   let torque = inferTorque forces in
-  let torque' = torque -  1000 * tank.angular.velocity in
-  let torque'' = torque'  - 1000 * tank.angular.velocity ^ 2 in
+  let torque' = torque - 600 * tank.angular.velocity in
+  let torque'' = torque' - 50 * tank.angular.velocity ^ 2 in
   let tank' = applyForce tank force dt in
   let tank'' = applyTorque tank' torque'' dt in
   tank'' |> Debug.watch "tank"
@@ -32,42 +34,42 @@ sign x = if x > 0 then 1 else if x < 0 then -1 else 0
 
 
 appliedForceToTorque : AppliedForce -> Float
-appliedForceToTorque {force, point} =
-  (sign point.x) * Vector.magnitude force * Vector.magnitude point
+appliedForceToTorque {force, point} = force × point
+
 
 inferTorque : List AppliedForce -> Float
 inferTorque appliedForces =
   appliedForces |> List.map appliedForceToTorque |> List.sum
 
 
-
-
-throttleToForce throttle = (toFloat throttle) * 1.0
+throttleToForce throttle = (toFloat throttle) * 15.0
 
 
 inferForces : Tank -> Controls -> List AppliedForce
 inferForces tank {tracks} =
   let angle = tank.angular.position in
-  [ { force=Vector.rotate { x=0, y=throttleToForce tracks.left } angle ,
-      point={ x=(-2 * tank.radius * sign tracks.left), y=0 } },
-      --point=Vector.rotate { x=(-2 * tank.radius), y=0 } angle },
-    { force=Vector.rotate { x=0, y=throttleToForce tracks.right } angle ,
-      point={ x=(2 * tank.radius * sign tracks.right), y=0 }},
-      --point=Vector.rotate { x=(2 * tank.radius), y=0 } angle },
+  [ { force=Vector.rotate { x=0, y=throttleToForce tracks.right } angle,
+      point=Vector.rotate { x=(-1 * tank.radius), y=0 } angle },
+    { force=Vector.rotate { x=0, y=throttleToForce tracks.left } angle,
+      point=Vector.rotate { x=(1 * tank.radius), y=0 } angle },
     { force=dragForce tank.velocity, point=Vector.zero },
-    { force=frictionForce tank.velocity, point=Vector.zero }
+    { force=frictionForce tank, point=Vector.zero }
   ]
 
 
-frictionForce : Vector -> Force
-frictionForce velocity =
-  let frictionCoefficient = 0.1 in
-  -1 * frictionCoefficient *. velocity
+frictionForce : Tank -> Force
+frictionForce {velocity, angular} =
+  let angle = angular.position in
+  let velocityAtTank = Vector.rotate velocity -angle in
+  let k = {x=1.00 , y=0.1} in
+  let friction = {velocityAtTank | x <- velocityAtTank.x * -k.x,
+                                   y <- velocityAtTank.y * -k.y} in
+  Vector.rotate friction angle
 
 
 dragForce : Vector -> Force
 dragForce velocity =
-  let dragCoefficient = 0.05 in
+  let dragCoefficient = 0.005 in
   -1 * dragCoefficient * (Vector.magnitude velocity) *. velocity
 
 
@@ -101,7 +103,7 @@ applyTorque ({angular} as tank) torque dt =
 
 -- SIGNALING
 input =
-  let delta = Signal.map (\t -> t/20) (fps 60) in
+  let delta = Signal.map (\t -> t / 1000) (fps 60) in
   Signal.sampleOn delta (Signal.map2 (,) delta Controls.all)
 
 main =
